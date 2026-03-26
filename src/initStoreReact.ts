7bf0a29e61456fd32e58ebb990171fdcf84b2cf6
Alexey Tolkachev <737217@gmail.com>
2026-03-26 17:49:10 +0300

    fix(initStoreReact): initState otions signature

--- src/initStoreReact.ts
+++ src/initStoreReact.ts
@@ -26,6 +26,14 @@ export function initStore<
 >(
   config: InitStoreConfig<S, A, Sel>,
   options?: InitStoreOptions,
+): InitializedReactStore<S, A, Sel>;
+export function initStore<
+  S extends object,
+  A extends Record<string, (...args: any[]) => any>,
+  Sel extends Record<string, (state: Readonly<S>) => any>,
+>(
+  config: InitStoreConfig<S, A, Sel>,
+  options?: InitStoreOptions,
 ): InitializedReactStore<S, A, Sel> {
   const store = initStoreBase(config, options);
   const useSelector = createUseSelector(store);
